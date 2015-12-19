fec486d5fcca8750228112151e6dd267d40afdaa
mohitsethi <mohit@sethis.in>
2015-12-20 02:43:16 +0530

    bumped to v0.1.1

--- lib/uptime/checks.rb
+++ lib/uptime/checks.rb
@@ -4,8 +4,21 @@ module Uptime
   class Checks
     # attr_accessor :username, :password
 
-    def self.all
+    def self.all(filter={})
       response = Uptime::Call.execute("api/checks", :get)
+    end
+
+    def self.create(options={})
+      Uptime::Call.execute("api/checks", :put, options)
+    end
+
+
+    def self.delete(check_id)
+      Uptime::Call.execute("api/checks#{check_id}", :delete)
+    end
+
+    def self.update(check_id, options={})
+      Uptime::Call.execute("api/checks#{check_id}", :post, options)
     end
 
   end
